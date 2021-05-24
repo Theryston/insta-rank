0accd4d11189510d7c4399529b64d2fdddbfd1c9
Theryston <funktodo2@gmail.com>
2021-05-24 01:18:45 +0000

    ajustes front-end

--- front-end/src/app/views/auth-instagram/auth-instagram.component.ts
+++ front-end/src/app/views/auth-instagram/auth-instagram.component.ts
@@ -16,33 +16,32 @@ export class AuthInstagramComponent implements OnInit {
   ngOnInit(): void {
     var user: any = localStorage.getItem('user')
     user = JSON.parse(user)
-    this.router.queryParams.subscribe(async (queryParams) => {
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        }
-        const url = "https://api.instagram.com/oauth/access_token"
-        const params = new URLSearchParams()
-        params.append('client_id', '260917165732736')
-        params.append('client_secret', 'b12a84e13f6f71a15351d97eab64ef61')
-        params.append('grant_type', 'authorization_code')
-        params.append('redirect_uri', 'https://4200-maroon-turkey-xsybm1cb.ws-us04.gitpod.io/auth')
-        params.append('code', queryParams.code)
-        const res = await axios.post(url, params, config)
-        console.log(res)
-        user.instagram_token = res.data.access_token
-        user.instagram_id = res.data.user_id
-        // this.userService.update(user).subscribe(() => {
-        //   localStorage.removeItem('user')
-        //   localStorage.setItem('user', JSON.stringify(user));
-        //   window.location.href = '/';
-        // })
-      } catch (err) {
-        console.log('>>>>>', err)
-      }
-    })
+    // this.router.queryParams.subscribe(async (queryParams) => {
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //       }
+    //     }
+    //     const url = "https://api.instagram.com/oauth/access_token"
+    //     const params = new URLSearchParams()
+    //     params.append('client_id', '260917165732736')
+    //     params.append('client_secret', 'b12a84e13f6f71a15351d97eab64ef61')
+    //     params.append('grant_type', 'authorization_code')
+    //     params.append('redirect_uri', 'https://4200-maroon-turkey-xsybm1cb.ws-us04.gitpod.io/auth')
+    //     params.append('code', queryParams.code)
+    //     const res = await axios.post(url, params, config)
+    //     user.instagram_token = res.data.access_token
+    //     user.instagram_id = res.data.user_id
+    //     // this.userService.update(user).subscribe(() => {
+    //     //   localStorage.removeItem('user')
+    //     //   localStorage.setItem('user', JSON.stringify(user));
+    //     //   window.location.href = '/';
+    //     // })
+    //   } catch (err) {
+    //     console.log('>>>>>', err)
+    //   }
+    // })
   }
 
 }
