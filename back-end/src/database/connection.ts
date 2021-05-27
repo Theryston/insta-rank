047bed9786629cbd348c25fd5204cfe91e63cd0a
Theryston <funktodo2@gmail.com>
2021-05-27 16:17:32 +0000

    remove bugs

--- back-end/src/database/connection.ts
+++ back-end/src/database/connection.ts
@@ -2,7 +2,6 @@ import { Sequelize } from 'sequelize';
 
 const connection = new Sequelize('rank_insta', 'prod', 'Senhagenerica1', {
   host: 'mysql743.umbler.com',
-  port: 41890,
   dialect: 'mysql'
 })
 
